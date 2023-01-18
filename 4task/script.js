bf3daaa78c7fde6238b2aa1a060f956953b8e48b
ratSeraphim <telma.laudobele@accenture.com>
2023-01-18 12:40:56 +0200

    Finished task 4

--- 4task/script.js
+++ 4task/script.js
@@ -1,1 +1,13 @@
 /*Html file with a submit button. Write a JavaScript function to get the value of the href, hreflang, rel, target, and type attributes of the specified link.*/
+
+function getAttributes(){
+    console.log("button clicked");
+    let aHref = document.getElementById("w3r").href;
+    let aHreflang = document.getElementById("w3r").hreflang;
+    let aRel = document.getElementById("w3r").rel;
+    let aTarget = document.getElementById("w3r").target;
+    let aType = document.getElementById("w3r").type;
+    let result = "Link's href: "+aHref+"\nHreflang: "+aHreflang+"\nRel: "+aRel+"\nTarget: "+aTarget+"\nType: "+aType;
+    console.log(result);
+    gainedValues.textContent = result;
+}
