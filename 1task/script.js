5272534ef069796943782677788e580538d92758
ratSeraphim <telma.laudobele@accenture.com>
2023-01-18 12:03:51 +0200

    Completed task 3

--- 1task/script.js
+++ 1task/script.js
@@ -1,5 +1,6 @@
 /* HTML file with a submit button. Now modify the style of the paragraph text through javascript code. */
 
 function js_style(){
+    console.log("button clicked");
     document.getElementById("text").style.color = "blue";
 };
---
+++ 3task/script.js
@@ -0,0 +1,6 @@
+/* Write a JavaScript program to set the background color of a paragraph. */
+
+function js_style(){
+    console.log("button clicked");
+    document.getElementById("text").style.background = "red";
+};
