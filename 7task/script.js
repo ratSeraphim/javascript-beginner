80fd10ded0463f8059f0e5206d6a8a943e316a5f
ratSeraphim <telma.laudobele@accenture.com>
2023-01-18 15:16:55 +0200

    Replaced for loops with foreach

--- 7task/script.js
+++ 7task/script.js
@@ -3,13 +3,28 @@
 function createTable(){
     var rownum = window.prompt("Row number");
     var colnum = window.prompt("Column number");
-    
-    for(var i = 0; i < rownum; i++ ){
+
+    //fills the arrays with numbers from 0 to 1 below length//
+    let rows = Array.from({length: rownum}, (elm, index)=> {
+        return index;
+    });
+    let columns = Array.from({length: colnum}, (elm, index)=> {
+        return index;
+    });
+
+    rows.forEach((rows ) => {
+        var r = document.getElementById('myTable').insertRow(rows);
+        columns.forEach((columns) => {
+            var c = r.insertCell(columns);
+            c.innerHTML = "row"+rows+ " col"+columns;
+        });
+    });
+   /* for(var i = 0; i < rownum; i++ ){
         var r = document.getElementById('myTable').insertRow(i);
         for(var j = 0; j < colnum; j++){
             var c = r.insertCell(j);
             c.innerHTML = "r"+i +" c"+j;
         } 
 
-    }
+    } */
 }
