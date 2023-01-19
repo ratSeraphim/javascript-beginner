eae45fb6fdf6cce2e6c61f866ee8abe271fca066
ratSeraphim <telma.laudobele@accenture.com>
2023-01-19 14:31:05 +0200

    Updated task 6

--- 6task/script.js
+++ 6task/script.js
@@ -1,8 +1,8 @@
 /* Write a JavaScript function that accept row, column, (to identify a particular cell) and a string to update the content of that cell. */
 
 function changeContent(){
-    const rownum = window.prompt("Row number");
-    const colnum = window.prompt("Column number");
-    const z = window.prompt ("Cell content");
-    document.getElementById('myTable').rows[rownum].cells[colnum].innerHTML = z;
+    const row = document.forms["selectors"]["row"].value;
+    const column = document.forms["selectors"]["col"].value;
+    const content = document.forms["selectors"]["content"].value;
+    document.getElementById('myTable').rows[row].cells[column].innerHTML = content;
 }
