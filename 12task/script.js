b1f8602627dc46d8b14b30660003e7c7abc279f3
ratSeraphim <telma.laudobele@accenture.com>
2023-01-19 13:48:38 +0200

    Completed task 12

---
+++ 12task/script.js
@@ -0,0 +1,18 @@
+/* Write a JavaScript program to highlight the bold words of the following paragraph, on mouse over. */
+function highlightText(){
+    console.log("Link hovered over");
+    const bolded = document.querySelectorAll('b');
+    bolded.forEach(element => {
+        element.style.color="gray";
+        element.style.background="yellow";
+    });
+}
+
+function unhighlightText(){
+    console.log("Mouse left link");
+    const bolded = document.querySelectorAll('b');
+    bolded.forEach(element => {
+        element.style.color="inherit";
+        element.style.background="inherit";
+    });
+}
